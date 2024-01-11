61fe36fdb2c8d6311d225a85ca88050d202fda3b
rezaul118255 <rezaul118255@gmail.com>
2024-01-11 09:01:04 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl text-purple-400 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
